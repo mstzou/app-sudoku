f31b5bda7bffe41609835c050f82174af12c304d
Miller <miller@millertzou.com>
2025-08-08 15:34:12 -0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 // next.config.ts
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "app-sudoku"; // <-- set your repo name
+// const isProd = process.env.NODE_ENV === "production";
+// const repoName = "app-sudoku"; // <-- set your repo name
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? `/${repoName}` : undefined,
-  assetPrefix: isProd ? `/${repoName}/` : undefined,
+  // basePath: isProd ? `/${repoName}` : undefined,
+  // assetPrefix: isProd ? `/${repoName}/` : undefined,
   images: { unoptimized: true },
 };
 
